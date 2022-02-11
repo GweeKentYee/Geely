7dc096181524bbc7578058c0dff54c7bb43adc25
GweeKentYee <kentyee2001@hotmail.my>
2022-02-11 16:37:53 +0800

    Inspection DataTable

--- resources/views/Inspection.blade.php
+++ resources/views/Inspection.blade.php
@@ -3,9 +3,54 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <h3><u>Inspection</u></h3>
+        <h3><u>Inspection</u></h3>
+        <div class="col-md-9">
+            <table class = "table" id = "datatable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Date</th>
+                        <th>File</th>
+                        <th>Used_Car_ID</th>
+                        <th>Action</th>
+                    </tr>
+                <thead>
+            </table>
         </div>
     </div>
 </div>
+@endsection
+
+@section('footer-scripts')
+<script>
+    $(document).ready(function () {
+            $('#datatable').DataTable({
+                "columnDefs": [{
+                    "defaultContent": "-",
+                    "targets": "_all"
+                }],
+                "processing": true,
+                "serverSide": true,
+                "ajax": "{{ route('api.inspection')}}",
+                "columns": [
+                    {"data": "id"},
+                    {"data": "inspection_date"},
+                    {"data": "file"},
+                    {"data": "used_car_id"},
+                    {"data": "Action", orderable: false, searchable: false}
+                ]
+            });
+
+            $('#datatable').on('click', '.delete', function () {
+
+                var confirmation = confirm('Delete the record?');
+
+                if (confirmation == false){
+                    return false;
+                }
+            });
+
+        });
+
+</script>
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/inspection','App\Http\Controllers\DataTableController@inspection')->name('api.inspection');
