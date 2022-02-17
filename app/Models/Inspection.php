7dc80ead7d49bf5cc2ca2f553474587a482e3ba5
GweeKentYee <kentyee2001@hotmail.my>
2022-02-17 14:10:33 +0800

    Updated Model content and migration files

--- app/Models/Inspection.php
+++ app/Models/Inspection.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Inspection extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'inspection_date',
+        'file',
+        'used_car_id'
+    ];
+
+    public function usedCar()
+    {
+        return $this->belongsTo('App\Models\UsedCar', 'used_car_id');
+    }
+
 }
--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Newsletter extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'image',
+        'sequence'
+    ];
 }
