3e68003e99e9fba4621dc157a466a44134b96f97
GweeKentYee <kentyee2001@hotmail.my>
2022-02-17 14:11:35 +0800

    some testing

--- app/Http/Controllers/InspectionController.php
+++ app/Http/Controllers/InspectionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Inspection;
+use App\Models\UsedCar;
 use Illuminate\Http\Request;
 
 class InspectionController extends Controller
@@ -13,6 +14,12 @@ class InspectionController extends Controller
     }
 
     public function viewAdminPage(){
+
+        // $latestinspection = Inspection::select('file')->where('used_car_id',1)->latest('inspection_date')->first();
+
+        // $HAHA = UsedCar::find(1);
+
+        // $inspection = $HAHA->inspections;
 
         return view('Inspection');
 
--- resources/views/Inspection.blade.php
+++ resources/views/Inspection.blade.php
@@ -5,6 +5,9 @@
     <div class="row justify-content-center">
         <h3><u>Inspection</u></h3>
         <div class="col-md-9">
+            <div style = "text-align:right" class = "pb-1">
+                <button class = "btn btn-primary" data-toggle="modal" data-target="#newinspection">New Inspection</button>
+            </div>
             <table class = "table" id = "datatable">
                 <thead>
                     <tr>
@@ -17,6 +20,57 @@
                 <thead>
             </table>
         </div>
+    </div>
+
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+               <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalCenterTitle">New Inspection</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                  </button>
+               </div>
+               <form action="#" method="post" enctype="multipart/form-data">
+                @csrf
+                    <div class="modal-body">
+                        <div class="playerfile">
+                            <label>File ( JSON, XML, Txt, PNG, JPEG ):</label>
+                            <input type = "file" name = "json/txt" id = "playerjson" class = "form-control-file @error('json/txt') is-invalid @enderror" accept = "application/JSON,application/xml,text/plain,text/xml,image/png,image/jpeg">
+                                @error('json/txt')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                                <br>
+                                <label>Type:</label>
+                                <input id="filetype" name = "file_type" type="text" class="form-control @error('file_type') is-invalid @enderror" value = "{{ old('file_type') }}" autocomplete="off" autofocus>
+                                @error('file_type')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                @enderror
+                                <br>
+                                <label>Permission:</label>
+                                <select id="filepermission" name = "permission" class = "form-control @error('permission') is-invalid @enderror">
+                                    <option value="0" disabled selected>-- Please Select Permission --</option>
+                                    <option value="private">Private</option>
+                                    <option value="global">Global</option>
+                                </select>
+                                    @error('permission')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Cancel</button>
+                        <input type="submit" class="btn btn-outline-success" id = "AddSubmit" value="Add">
+                    </div>
+               </form>
+            </div>
+         </div>
     </div>
 </div>
 @endsection
