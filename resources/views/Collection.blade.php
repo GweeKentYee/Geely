eb52efa4ea4b1d61f8f9fd0cd23d81c8823a24ba
Seow Yong Tao <seowyongtao22@gmail.com>
2022-02-25 17:19:28 +0800

    Add pop up modal for delete confirmation

--- resources/views/Collection.blade.php
+++ resources/views/Collection.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
 {{--  <div class="container">  --}}
+    
     <div class="row">
         <div class="col-9">
             <h3 class="headline">COLLECTION</h3>
@@ -27,7 +28,7 @@
         <div class="col-2 title-bar-column title-bar-rating-column">RATING</div>
         <div class="col-1">
         </div>
-</div>
+    </div>
     @foreach ($collections as $collection)
         <div class="row collection-container">
                 <div class="col-1 m-auto">
@@ -39,20 +40,52 @@
                 <div class="col-2 m-auto">RM{{ $collection->min_price }}-RM{{ $collection->max_price }}</div>
                 <div class="col-2 m-auto">{{ $collection->id }}</div>
                 <div class="col-1 m-auto">
-                    <form action="{{ route('collection.destroy', ['collection' => $collection->id]) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <input class="bg-danger text-light rounded shadow" type="submit" value="X">
-                    </form>
+                    <button type="button" class="btn btn-danger shadow" data-toggle="modal" data-target="#exampleModal{{ $collection->id }}">
+                        X
+                    </button>
+                    <div class="modal fade" id="exampleModal{{ $collection->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <form action="{{ route('collection.destroy', ['collection' => $collection->id]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Remove collection</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div class="modal-body">
+                                Are you sure you want to remove this collection?
+                                </div>
+                                <div class="modal-footer">                                 
+                                    <button type="submit" class="btn btn-success">Yes</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
+                                </div>
+                            </form>
+                          </div>
+                        </div>
+                    </div>
                 </div>
         </div>
+        <div class="row collection-container">
+            <div class="col-1"></div>
+            <div class="col-10"><hr></div>
+            <div class="col-1"> </div>
+        </div>
+     
     @endforeach
 
 {{--  </div>  --}}
 @endsection
 
 @section("footer-scripts")
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script>
+        
+            
             $(document).ready(function () {
                 $('input[type=checkbox]').on('change', function (e) {
                     if ($('input[type=checkbox]:checked').length > 2) {
