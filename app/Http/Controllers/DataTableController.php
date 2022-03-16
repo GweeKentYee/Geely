f1c049c3dd3531a22413d07ac81ecf847759e6d6
mazbrew <peteryeoh50@gmail.com>
2022-03-16 21:08:35 +0800

    merged with kent

--- app/Http/Controllers/DataTableController.php
+++ app/Http/Controllers/DataTableController.php
@@ -44,24 +44,4 @@ class DataTableController extends Controller
             ->make(true);
 
     }
-
-    public function ManageCatalogue(){
-        $query = Catalogue::select('*');
-
-        return datatables($query)
-            ->addIndexColumn()
-                ->addColumn('Action', function($query){
-
-                    $actionBtn = //'<a href = "/player/download/' .$query->JSON_file. '" class = "download btn btn-primary btn-sm">Download</a>
-                                    //'<a class = "btn btn-success btn-sm edit" href = "/allplayer/edit/'.$query->id.'">Edit</a>
-                                    '<a class= "btn btn-primary btn-sm details" href= "#" >Details</a>
-                                    <a class= "btn btn-success btn-sm upload" href= "#" >Upload</a>
-                                    <a class= "btn btn-danger btn-sm delete" href= "/admin/catalogue/delete/'.$query->id.'">Delete</a>'
-                                    ;
-                    return $actionBtn;
-
-                })->rawColumns(['Action'])
-                ->make(true);
-
-    }
 }
