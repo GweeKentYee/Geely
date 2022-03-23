bc973d62dea6c6d3985f8bd593b2483ad5003e10
yijingLim <hcyyl9@nottingham.edu.my>
2022-03-23 12:18:50 +0800

    "Updates"

--- app/Http/Controllers/UsedCarController.php
+++ app/Http/Controllers/UsedCarController.php
@@ -6,7 +6,5 @@ use Illuminate\Http\Request;
 
 class UsedCarController extends Controller
 {
-    public function viewPage(){
-        return view('UsedCarDetails');
-    }
+    //
 }
---
+++ database/migrations/2022_01_29_020819_create_catalogues_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCataloguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalogues', function (Blueprint $table) {
+            $table->id();
+            $table->integer('min_price');
+            $table->integer('max_price');
+            $table->unsignedBigInteger('used_car_id')->required();
+            $table->timestamps();
+
+            $table->foreign('used_car_id')->references('id')->on('used_cars')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalogues');
+    }
+}
