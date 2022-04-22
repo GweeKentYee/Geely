a7c3471c5de1507060c7107cb975e273732865b7
GweeKentYee <kentyee2001@hotmail.my>
2022-04-22 09:31:55 +0800

    Admin Controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+    //
+    protected function adminRegisterPage(){
+
+        return view('auth/registerAdmin');
+
+    }
+
+    protected function registerAdmin(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'status' => "Admin",
+            'password' => Hash::make($data['password']),
+
+        ]);
+
+        return redirect('/admin/inspection');
+    }
+}
