d928a5441cd855227aa5a247fc884103ac1cf2be
GweeKentYee <kentyee2001@hotmail.my>
2022-04-27 08:04:33 +0800

    Car Details Image

--- resources/views/UsedCarDetails.blade.php
+++ resources/views/UsedCarDetails.blade.php
@@ -13,8 +13,14 @@
                     <div class="row">
                         <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-4">
 
-                            @if (!empty($usedcars->usedCarImages[0]->image))
+                            @if (empty($usedcars->usedCarImages[0]->image))
 
+                                <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="black" class="bi bi-images" viewBox="0 0 16 16">
+                                    <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                                    <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+                                </svg>
+
+                            @else
                                 {{-- Carousel slides --}}
                                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
 
@@ -40,11 +46,6 @@
                                     </a>
                                 </div>
                                 {{-- end of carousel slides--}}
-                            @else
-                                <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="black" class="bi bi-images" viewBox="0 0 16 16">
-                                    <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-                                    <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
-                                </svg>
                             @endif
                         </div>
                         <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-8">
